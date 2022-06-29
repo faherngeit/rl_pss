4ccfb524498bdae798528f2ab81dae42dd5ed6c3
faherngeit <valeriy.tashchilin@hey.com>
2022-06-29 09:30:50 +0500

    Clear root directory

--- default_state.m
+++
@@ -1,7 +0,0 @@
-function x = default_state()
-x.signals.values = 0;
-x.signals.dimensions = 1;
-x.signals.label = 'CSTATE';
-x.signals.blockName = 'simmodel/Integrator';
-x.signals.stateName = '';
-x.signals.inReferencedModel = false;
