7f5e6a75048cde0e7f8c45bb5482630313d52ab7
Mikhail Shakirov <m.m.shakirov@inbox.ru>
2022-08-24 04:01:15 +0500

    Move postfix to constant

--- simulation_test/send_result.py
+++ simulation_test/send_result.py
@@ -2,6 +2,7 @@ import requests
 
 
 AGENT_ADDRESS = "127.0.0.1:8001"
+POSTFIX = "/predict/"
 
-response = requests.post("http://" + AGENT_ADDRESS + "/predict/",
+response = requests.post("http://" + AGENT_ADDRESS + POSTFIX,
                           json = pyJsonText)
