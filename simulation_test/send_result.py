188723d03e30e1cfbdca305b03f77025e44ab1a3
Mikhail Shakirov <m.m.shakirov@inbox.ru>
2022-08-25 22:31:53 +0500

    Fix postfix

--- simulation_test/send_result.py
+++ simulation_test/send_result.py
@@ -10,7 +10,7 @@ CONFIG_PATH = "general_config.json"
 config = AgentDescription.from_file(CONFIG_PATH)
 
 AGENT_ADDRESS = config.agentHost + ":" + str(config.agentPort)
-POSTFIX = "/" + config.trainPostfix
+POSTFIX = config.trainPostfix
 
 response = requests.post("http://" + AGENT_ADDRESS + POSTFIX,
                           json = pyJsonText)
