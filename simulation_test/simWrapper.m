5bbfd203193dabeb6851cc4b699f246b8a793ae3
Mikhail Shakirov <m.m.shakirov@inbox.ru>
2022-08-27 15:00:49 +0500

    Connect post-processing to result sender

--- simulation_test/simWrapper.m
+++ simulation_test/simWrapper.m
@@ -1,13 +1,7 @@
-function pyOut = simWrapper(scenarios_file, reward_type, penalty)
- % Выбор сценария, моделирование, расчет награды
- out = getScenarioSimOutData(scenarios_file, reward_type, penalty)
+function serverResponse = simWrapper(scenarios_file, reward_type, penalty)
+% Выбор сценария, моделирование, расчет награды
+result = getScenarioSimOutData(scenarios_file, reward_type, penalty);
 
- % Приведение к двумерному массиву для передачи в python без коллизий.
- %
- % Временное решение на время тестов (годится только для вызова 1 сценария),
- % позже нужно будет расширить, чтобы можно было передавать на обучение
- % результаты моделирования сразу нескольких сценариев.
- pyOut = [];
- for i = 1 : size(out, 3)
-     pyOut = [pyOut; out(:,:,i)];
- end
+% Запакова в JSON и отправка на сервер-агент
+serverResponse = sendResultWrapper(result);
+end
