1c87d885a623230294c1d37a5f30166a72bed045
Mikhail Shakirov <m.m.shakirov@inbox.ru>
2022-09-16 02:51:13 +0500

    Added tests

---
+++ matlab_env/testSimWrapperIntMaxDeltaWs.m
@@ -0,0 +1,6 @@
+function testSimWrapperIntMaxDeltaWs(pathToLog)
+response = simWrapper("ten_almost_similar", "IntMaxDeltaWs", 1000, 1, pathToLog, pathToLog);
+assert(response.StatusLine.StatusCode == matlab.net.http.StatusLine('HTTP/1.1 200 OK').StatusCode, ...
+    "TEST FAILED. Error in POST /train/ with status code %i", response.StatusLine.StatusCode)
+disp("TEST PASSED");
+end
